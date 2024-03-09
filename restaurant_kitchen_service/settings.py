89c29c6e30509b1ae2733f3eb583e73b3b5f9625
Maksym Operchuk <operlast529@gmail.com>
2024-03-09 15:42:21 +0200

    update settings & requirements.txt

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.contrib import staticfiles
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vstd#-*=y4q192vr%4!_*k+)-wjn#!8hdkz$wzwu4m$ea87^x1"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-vstd#-*=y4q192vr%4!_*k+)-wjn#!8hdkz$wzwu4m$ea87^x1")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
