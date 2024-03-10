3137ded1c7c1442f5392ce0adc0d25a4df586cdb
Maksym Operchuk <operlast529@gmail.com>
2024-03-10 19:24:10 +0200

    remove insecure django secret key for deployment

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-vstd#-*=y4q192vr%4!_*k+)-wjn#!8hdkz$wzwu4m$ea87^x1")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
@@ -87,7 +87,6 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = "postgres://lyukgfym:9uU2xB2aRoQL1ZsWPAhHBuuqfpFpHo8z@cornelius.db.elephantsql.com/lyukgfym"
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
