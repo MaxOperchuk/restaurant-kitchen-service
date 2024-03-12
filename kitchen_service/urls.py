d4e51d6221d3ba90304c8610ae2a63e03df5cdc4
Maksym Operchuk <operlast529@gmail.com>
2024-03-12 09:41:15 +0200

    rename endpoints in plural form.

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -19,17 +19,49 @@ from kitchen_service.views import (
 urlpatterns = [
     path("", index, name="index"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
-    path("dishs/create/", DishCreateView.as_view(), name="dish-create"),
-    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
-    path("dish/<int:pk>/detail/", DishDetailView.as_view(), name="dish-detail"),
-    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
+    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
+    path(
+        "dishes/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update"
+    ),
+    path(
+        "dishes/<int:pk>/detail/",
+        DishDetailView.as_view(),
+        name="dish-detail"
+    ),
+    path(
+        "dishes/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete"
+    ),
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
-    path("dish-type/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
-    path("dish-type/<int:pk>/create/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
-    path("dish-type/<int:pk>/deatail/", DishTypeDetailView.as_view(), name="dish-type-detail"),
-    path("dish-type/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path(
+        "dish-types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create"
+         ),
+    path(
+        "dish-types/<int:pk>/create/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete"
+    ),
+    path(
+        "dish-types/<int:pk>/deatail/",
+        DishTypeDetailView.as_view(),
+        name="dish-type-detail"
+         ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update"
+    ),
     path("cooks/", CookListView.as_view(), name="cook-list"),
-    path("cook/<int:pk>/delete/", CookDeleteView.as_view(), name="cook-delete"),
+    path(
+        "cooks/<int:pk>/delete/",
+        CookDeleteView.as_view(),
+        name="cook-delete"
+    ),
 
 ]
 
