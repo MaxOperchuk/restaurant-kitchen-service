18097c51a3dcc10bb8d1f0faac502cc26a3d046b
Maksym Operchuk <operlast529@gmail.com>
2024-03-12 09:40:29 +0200

    add minValueValidator to price field in Dish model

--- kitchen_service/models.py
+++ kitchen_service/models.py
@@ -1,33 +1,25 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class DishType(models.Model):
-    name = models.CharField(
-        max_length=255,
-    )
-    description = models.TextField(
-        null=True,
-        blank=True,
-    )
-    country = models.CharField(
-        null=True,
-        max_length=65
-    )
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    country = models.CharField(null=True, max_length=65)
 
     def __str__(self):
         return self.name
 
 
 class Dish(models.Model):
-    name = models.CharField(
-        max_length=255,
-    )
+    name = models.CharField(max_length=255,)
     description = models.TextField()
     price = models.DecimalField(
         max_digits=6,
         decimal_places=2,
+        validators=[MinValueValidator(0.01)]
     )
     dish_type = models.ForeignKey(
         DishType,
